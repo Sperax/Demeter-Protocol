68314633a4d04a5048d54672dc2de70471ee7e76
YashP16 <yash.pitroda786@gmail.com>
2022-08-18 14:48:27 +0530

    refactor(scripts): Make frontend_setup script generic.

--- scripts/frontend_setup.py
+++ scripts/frontend_setup.py
@@ -53,38 +53,58 @@ def print_stats(farm, user_account, deposit_id):
             )
 
 
+def addr_to_token(addr):
+    name_dict = {
+        '0x5575552988A3A80504bBaeB1311674fCFd40aD4B': 'spa',
+        '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'usdc',
+        '0xD74f5255D557944cf7Dd0E45FF521520002D5748': 'usds',
+        '0x2CaB3abfC1670D1a452dF502e216a66883cDf079': 'l2dao',
+    }
+    return name_dict[addr]
+
+
 def token_obj(token):
     """Add the token contract interface with the correct address"""
+    if('0x' in token):
+        token = addr_to_token(token)
+
     token_dict = {
-        'spa': interface.IERC20('0x5575552988A3A80504bBaeB1311674fCFd40aD4B'),
-        'usdc': interface.IERC20('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'),
-        'usds': interface.IERC20('0xD74f5255D557944cf7Dd0E45FF521520002D5748'),
+        'spa': interface.ERC20('0x5575552988A3A80504bBaeB1311674fCFd40aD4B'),
+        'usdc': interface.ERC20('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'),
+        'usds': interface.ERC20('0xD74f5255D557944cf7Dd0E45FF521520002D5748'),
+        'l2dao': interface.ERC20(
+            '0x2CaB3abfC1670D1a452dF502e216a66883cDf079'
+        ),
     }
     return token_dict[token]
 
 
 def funds(token):
     """"Add the address to be used for funding the user wallet and vault"""
+    if('0x' in token):
+        token = addr_to_token(token)
     fund_dict = {
         'spa': '0xda7ed5d425640c96dc52cc5fae8218f99450df95',
         'usds': '0x3944b24f768030d41cbcbdcd23cb8b4263290fad',
-        'usdc': '0x1714400ff23db4af24f9fd64e7039e6597f18c2b'
+        'usdc': '0x1714400ff23db4af24f9fd64e7039e6597f18c2b',
+        'l2dao': '0x5318f07a3a20a2f8bb0ddf14f1dd58c517a76508'
     }
     return fund_dict[token]
 
 
-def fund_account(user, token_name, amount):
+def fund_account(user, token, amount):
     """Function to fund a wallet and approve tokens for vault to spend"""
-
-    token = token_obj(token_name)
+    token_name = token
+    if(hasattr(token, 'address')):
+        token_name = token.address
+    else:
+        token = token_obj(token)
     token.transfer(user, amount, {'from': funds(token_name)})
 
 
 def main():
     owner = accounts[0]
 
-    spa = token_obj('spa')
-    usds = token_obj('usds')
     user_account = input('Enter your metamask wallet address: ')
     use_pre_deployed = input('Use pre-deployed contract? (y/n): ')
 
@@ -107,18 +127,21 @@ def main():
 
     print(f'\n{network.show_active()}:\n')
     print(f'{config_name} Farm address: {farm.address}')
+    reward_data = list(
+        map(lambda x: list(x.values()), config['reward_token_data'])
+    ),
+    token_A = token_obj(config['uniswap_pool_data']['token_A'])
+    token_B = token_obj(config['uniswap_pool_data']['token_B'])
+    rwd_tokens = map(lambda x: token_obj(x[0][0]), reward_data)
 
     accounts[0].transfer(user_account, '50 ether')
 
-    fund_account(owner, 'usds', 2e23)
-    fund_account(owner, 'spa', 2e23)
-    fund_account(user_account, 'usds', 1e22)
-    fund_account(user_account, 'spa', 1e22)
+    for token in rwd_tokens:
+        fund_account(owner, token, 2e23)
+        token.approve(farm, 1e6 * 10**token.decimals(), {'from': owner})
+        farm.addRewards(token, 1e5 * 10**token.decimals(), {'from': owner})
 
-    usds.approve(farm, 1e24, {'from': owner})
-    spa.approve(farm, 1e24, {'from': owner})
-
-    farm.addRewards(usds, 1e23, {'from': owner})
-    farm.addRewards(spa, 1e23, {'from': owner})
+    fund_account(user_account, token_A, 1e4 * 10 ** token_A.decimals())
+    fund_account(user_account, token_B, 1e4 * 10 ** token_B.decimals())
 
     chain.snapshot()
