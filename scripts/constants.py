c579df6ef027983b4a3b4e76745533c7d6e51afe
YashP16 <yash.pitroda786@gmail.com>
2022-08-18 14:47:56 +0530

    feat(scripts): Add deployment config for l2Dao-Usds farm.

--- scripts/constants.py
+++ scripts/constants.py
@@ -5,6 +5,34 @@ from brownie import (
 )
 
 constants = {
+    'l2dao_usds': {
+        'contract': Farm,
+        'config': {
+            'farm_start_time': chain.time() + (86400 * 1),
+            'cooldown_period': 0,
+            'uniswap_pool_data': {
+                'token_A': '0x2CaB3abfC1670D1a452dF502e216a66883cDf079',
+                'token_B': '0xD74f5255D557944cf7Dd0E45FF521520002D5748',
+                'fee_tier': 3000,
+                'lower_tick': -48960,
+                'upper_tick': -6900,
+            },
+            'reward_token_data': [
+                {
+                   'reward_tkn': '0x2CaB3abfC1670D1a452dF502e216a66883cDf079',
+                   'tkn_manager': '0x5b12d9846F8612E439730d18E1C12634753B1bF1',
+                   'emergency_r': '0xb56e5620A79cfe59aF7c0FcaE95aADbEA8ac32A1',
+                   'reward_rate': [1e15],
+                },
+                {
+                   'reward_tkn': '0x5575552988A3A80504bBaeB1311674fCFd40aD4B',
+                   'tkn_manager': '0x5b12d9846F8612E439730d18E1C12634753B1bF1',
+                   'emergency_r': '0xb56e5620A79cfe59aF7c0FcaE95aADbEA8ac32A1',
+                   'reward_rate': [1e15],
+                },
+            ]
+        }
+    },
     'test_farm_with_lockup': {
         'contract': Farm,
         'config': {
@@ -21,13 +49,13 @@ constants = {
                 {
                    'reward_tkn': '0xD74f5255D557944cf7Dd0E45FF521520002D5748',
                    'tkn_manager': accounts[0].address,
-                   'emergency_r': accounts[1].address,
+                   'emergency_r': '0xb56e5620A79cfe59aF7c0FcaE95aADbEA8ac32A1',
                    'reward_rate': [1e13, 5e12],
                 },
                 {
                    'reward_tkn': '0x5575552988A3A80504bBaeB1311674fCFd40aD4B',
                    'tkn_manager': accounts[0].address,
-                   'emergency_r': accounts[1].address,
+                   'emergency_r': '0xb56e5620A79cfe59aF7c0FcaE95aADbEA8ac32A1',
                    'reward_rate': [1e15, 5e14],
                 },
             ]
