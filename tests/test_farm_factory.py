eb56c3313748eff937afd67d6c1cd08ce8acf45f
Mehul Tuteja <reachmehultuteja@gmail.com>
2022-09-19 00:24:50 +0530

    100% code coverage for FarmFactory.sol

--- tests/test_farm_factory.py
+++ tests/test_farm_factory.py
@@ -5,12 +5,14 @@ from brownie import (
     accounts,
     UniswapFarmV1Deployer,
     reverts,
-    ZERO_ADDRESS
+    ZERO_ADDRESS,
+    UniswapFarmV1
 )
 import pytest
 import eth_utils
 from conftest import (
     GAS_LIMIT,
+    fund_account,
     token_obj
 )
 
@@ -45,6 +47,7 @@ def factory_contract():
 @pytest.fixture(scope='module')
 def factory(factory_contract):
     factory_contract.initialize(
+        deployer,
         token_obj('usds'),
         500e18,
         {'from': deployer}
@@ -56,27 +59,39 @@ def factory(factory_contract):
 def farm_deployer(factory):
     farm_deployer = UniswapFarmV1Deployer.deploy(factory, {'from': deployer})
     factory.registerFarmDeployer(
-        'UniswapFarmV1Deployer',
         farm_deployer,
         {'from': deployer}
     )
     return farm_deployer
 
 
-@pytest.mark.skip()
-class TestMain:
-    def test_main(self, farm_deployer):
-        pass
+@pytest.fixture(scope='module')
+def farm():
+    farm = UniswapFarmV1.deploy({'from': deployer})
+    return farm
 
 
-# @pytest.mark.skip()
 class TestInitialization:
-    def test_initialization_invalid_address(self, factory_contract):
+    def test_initialization_invalid_token(self, factory_contract):
+        print('-------------------------------------------------')
+        print('Trying to initialize with feeToken as invalid address')
+        with reverts('Invalid address'):
+            factory_contract.initialize(
+                deployer,
+                ZERO_ADDRESS,
+                500e18,
+                {'from': deployer}
+            )
+        print('Transaction reverted with: Invalid address')
+        print('-------------------------------------------------')
+
+    def test_initialization_invalid_receiver(self, factory_contract):
         print('-------------------------------------------------')
         print('Trying to initialize with feeToken as invalid address')
         with reverts('Invalid address'):
             factory_contract.initialize(
                 ZERO_ADDRESS,
+                token_obj('usds'),
                 500e18,
                 {'from': deployer}
             )
@@ -86,13 +101,14 @@ class TestInitialization:
     def test_initialization_zero_fees(self, factory_contract):
         print('-------------------------------------------------')
         print('Trying to initialize with feeAmount as 0')
-        with reverts('Fee cannot be zero'):
+        with reverts('Fee can not be 0'):
             factory_contract.initialize(
+                deployer,
                 token_obj('usds'),
                 0,
                 {'from': deployer}
             )
-        print('Transaction reverted with: Fee cannot be zero')
+        print('Transaction reverted with: Fee can not be 0')
         print('-------------------------------------------------')
 
     def test_initialization(self, factory):
@@ -100,16 +116,13 @@ class TestInitialization:
         print('Testing proper initialization')
         assert factory.feeToken() == token_obj('usds')
         assert factory.feeAmount() == 500e18
-        receiver = '0x5b12d9846F8612E439730d18E1C12634753B1bF1'
-        assert factory.FEE_RECEIVER() == receiver
+        assert factory.feeReceiver() == deployer
 
 
-# @pytest.mark.skip()
 class TestRegisterFarmDeployer:
     def test_only_admin(self, factory):
         with reverts('Ownable: caller is not the owner'):
             factory.registerFarmDeployer(
-                'AaveFarm',
                 accounts[1],
                 {'from': accounts[1]}
             )
@@ -117,22 +130,140 @@ class TestRegisterFarmDeployer:
     def test_zero_address(self, factory):
         with reverts('Invalid address'):
             factory.registerFarmDeployer(
-                'AaveFarm',
                 ZERO_ADDRESS,
-                {'from': accounts[0]}
+                {'from': deployer}
             )
 
-    def test_registerFarmDeployer(self, factory, farm_deployer):
-        factory.registerFarmDeployer(
-            'AaveFarm',
+    def test_registerFarmDeployer(self, factory):
+        tx = factory.registerFarmDeployer(
             accounts[1],
-            {'from': accounts[0]}
+            {'from': deployer}
         )
+        event = tx.events['FarmDeployerRegistered']
+        assert event['deployer'] == accounts[1]
+        assert factory.deployerRegistered(accounts[1])
 
-    def test_register_multiple_deployers(self, factory, farm_deployer):
-        with reverts('Deployer already exists'):
-            factory.registerFarmDeployer(
-                'UniswapFarmV1Deployer',
-                accounts[1],
-                {'from': accounts[0]}
+    def test_registerFarmDeployer_multiple(self, factory):
+        for i in range(2, 9):
+            tx = factory.registerFarmDeployer(
+                accounts[i],
+                {'from': deployer}
             )
+            event = tx.events['FarmDeployerRegistered']
+            assert event['deployer'] == accounts[i]
+            assert factory.deployerRegistered(accounts[i])
+
+    def test_register_same_deployer_twice(self, factory):
+        factory.registerFarmDeployer(
+            accounts[1],
+            {'from': deployer}
+        )
+        with reverts('Deployer already registered'):
+            factory.registerFarmDeployer(
+                accounts[1],
+                {'from': deployer}
+            )
+
+
+class TestRemoveDeployer:
+    def test_only_admin(self, factory):
+        with reverts('Ownable: caller is not the owner'):
+            factory.removeDeployer(
+                0,
+                {'from': accounts[1]}
+            )
+
+    def test_invalid_deployer_id(self, factory):
+        factory.registerFarmDeployer(
+            accounts[1],
+            {'from': deployer}
+        )
+        with reverts('Invalid deployer id'):
+            factory.removeDeployer(
+                1,
+                {'from': deployer}
+            )
+
+    def test_removeDeployer(self, factory):
+        for i in range(1, 5):
+            factory.registerFarmDeployer(
+                accounts[i],
+                {'from': deployer}
+            )
+        tx = factory.removeDeployer(
+            0,
+            {'from': deployer}
+        )
+        event = tx.events['FarmDeployerRemoved']
+        assert event['deployer'] == accounts[1]
+        assert factory.deployerList(0) == accounts[4]
+        for i in range(2):
+            assert factory.deployerList(i+1) == accounts[i+2]
+
+
+class TestRegisterFarm:
+    def test_unregisteredDeployer(self, factory, farm):
+        with reverts('Deployer not registered'):
+            factory.registerFarm(
+                farm,
+                accounts[5],
+                True,
+                {'from': accounts[5]}
+            )
+
+    def test_registerFarm_no_fees(self, factory, farm):
+        factory.registerFarmDeployer(
+            accounts[1],
+            {'from': deployer}
+        )
+        tx = factory.registerFarm(
+            farm,
+            accounts[1],
+            False,
+            {'from': accounts[1]}
+        )
+        event = tx.events['FarmRegistered']
+        assert farm == event['farm']
+        assert accounts[1] == event['creator']
+        assert factory.farms(0) == farm
+        assert factory.farmRegistered(farm)
+
+    def test_registerFarm_with_fees(self, factory, farm):
+        fund_account(accounts[2], 'usds', 500*1e18)
+        token_obj('usds').approve(
+            factory,
+            500*1e18,
+            {'from': accounts[2]}
+        )
+        factory.registerFarmDeployer(
+            accounts[2],
+            {'from': deployer}
+        )
+        tx = factory.registerFarm(
+            farm,
+            accounts[2],
+            True,
+            {'from': accounts[2]}
+        )
+        event = tx.events['FarmRegistered']
+        assert farm == event['farm']
+        assert accounts[2] == event['creator']
+        assert factory.farms(0) == farm
+        assert factory.farmRegistered(farm)
+
+
+class TestUpdateFeeParams:
+    def test_updateFeeParams(self, factory):
+        tx = factory.updateFeeParams(
+            accounts[3],
+            token_obj('usdc'),
+            100*1e18,
+            {'from': deployer}
+        )
+        event = tx.events['FeeParamsUpdated']
+        assert event['receiver'] == accounts[3]
+        assert event['token'] == token_obj('usdc')
+        assert event['amount'] == 100*1e18
+        assert factory.feeReceiver() == accounts[3]
+        assert factory.feeToken() == token_obj('usdc')
+        assert factory.feeAmount() == 100*1e18
