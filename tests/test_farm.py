d75dfec237b988a8ac78e19bdc089f907fa77498
Mehul Tuteja <reachmehultuteja@gmail.com>
2022-09-21 22:40:38 +0530

    Template including admin and view fun's

--- tests/test_farm.py
+++ tests/test_farm.py
@@ -10,6 +10,9 @@ from conftest import (
     token_obj,
 )
 
+# import scripts.deploy_farm as farm_deployer
+# from ..scripts.constants import demeter_farm_constants
+
 farm_names = ['test_farm_with_lockup', 'test_farm_without_lockup']
 global farm, token_A, token_B, rwd_token
 
@@ -46,6 +49,10 @@ def test_intitialization():
 
 class Test_admin_function:
     class Test_update_cooldown:
+        def test_updateCooldownPeriod_only_admin(self):
+            # Should revert when called with a non owner account
+            pass
+
         def test_farm_with_no_cooldown(self):
             # Should revert
             pass
@@ -57,40 +64,174 @@ class Test_admin_function:
         def test_cooldown(self):
             pass
 
-    class Test_toggle_deposit:
-        def test_unauthorized_call(self):
+    class Test_update_farm_start_time:
+        def test_updateFarmStartTime_only_admin(self):
+            # Should revert when called with a non owner account
             pass
 
-        def test_toggle(self):
+        def test_updateFarmStartTime_for_started_farm(self):
+            # Should revert if farm is already started
             pass
 
-    class Test_declare_emergency:
-        def test_unauthorized_call(self):
+        def test_updateFarmStartTime_in_past(self):
+            # Should revert when _newStartTime is lesser than now
             pass
 
-        def test_declare_emergency(self):
+        def test_updateFarmStartTime(self):
+            # Should pass when farmStartTime is > now and _newStartTime > now
             pass
 
-    class Test_recover_reward_funds:
-        def test_unauthorized_call(self):
+    class Test_add_reward_token:
+        def test_addRewardToken_only_admin(self):
+            # Should revert when called with a non owner account
             pass
 
-        def test_recover_reward_funds(self):
+        def test_addRewardToken_more_than_max(self):
+            # Should revert when we try to add more than MAX_NUM_REWARDS
             pass
 
-    class Test_set_reward_rate:
-        def test_unauthorized_call(self):
+        def test_addRewardToken_invalid_token(self):
+            # Should revert When token is a zero address
             pass
 
-        def test_set_reward_rate(self):
+        def test_addRewardToken_invalid_manager(self):
+            # Should revert When token manager is a zero address
             pass
 
-    class Test_add_rewards:
-        def test_invalid_reward(self):
+        def test_addRewardToken_already_registered(self):
+            # Should revert when trying to register an already registered token
             pass
 
-        def test_add_rewards(self):
+        def test_addRewardToken_spa(self):
+            # Should skip token manager passed and set SPA_TOKEN_MANAGER
+            # from the constants of the contract
             pass
+
+        def test_addRewardToken(self):
+            # Should set data properly for tokens other than SPA
+            pass
+
+    class Test_farm_pause_switch:
+        def test_farmPauseSwitch_only_admin(self):
+            # Should revert when called with a non owner account
+            pass
+
+        def test_farmPauseSwitch_try_false(self):
+            # Should revert when isPaused is already paused and
+            # we again pass false
+            pass
+
+        def test_farmPauseSwitch_pause(self):
+            # Should pause the farm when passed _isPaused as true and
+            # other functions which use notPaused modifier should revert
+            pass
+
+        def test_farmPauseSwitch_unpause(self):
+            # unpause a paused farm and try to do operations normally
+            pass
+
+    class Test_close_farm:
+        def test_closeFarm_only_admin(self):
+            # Should revert when called with a non owner account
+            pass
+
+        def test_closeFarm(self):
+            # Should close farm and other functions using farmNotClosed
+            # modifier should revert
+            pass
+
+    class Test_recover_ERC20:
+        def test_recoverERC20_only_admin(self):
+            # Should revert when called by a non owner account
+            pass
+
+        def test_recoverERC20_reward_token(self):
+            # Should revert when owner tries to withdraw an added reward token
+            pass
+
+        def test_recoverERC20_zero_balance(self):
+            # Should revert when owner tries to withdraw a token
+            # whose balance is 0
+            pass
+
+        def test_recoverERC20(self):
+            # Owner should be able to withdraw any other ERC20 apart from
+            # reward tokens, sitting in the farm.
+            pass
+
+
+class Test_view_functions:
+    class Test_compute_rewards:
+        def test_computeRewards_invalid_deposit(self):
+            # Should revert if the deposit is invalid
+            pass
+
+        def test_computeRewards_before_farm_starts(self):
+            # No rewards should be accrued
+            pass
+
+        def test_computeRewards_after_farm_starts(self):
+            # Normal case
+            pass
+
+    def test_getNumDeposits(self):
+        pass
+
+    def test_getDeposit(self):
+        pass
+
+    def test_getNumSubscriptions(self):
+        pass
+
+    class Test_get_subscription_info:
+        def test_getSubscriptionInfo_invalid_subscriptio(self):
+            # Should revert with subscription does not exist
+            pass
+
+        def test_getSubscriptionInfo(self):
+            pass
+
+    def test_getRewardRates(self):
+        pass
+
+    def test_getRewardFundInfo(self):
+        pass
+
+    class Test_get_reward_balance:
+        def test_getRewardBalance_invalid_rwdToken(self):
+            # Should revert when _rwdToken is invalid
+            pass
+
+        def test_getRewardBalance_rewardsAcc_more_than_supply(self):
+            pass
+
+        def test_getRewardBalance(self):
+            # normal case
+            pass
+
+
+class Test_recover_reward_funds:
+    def test_unauthorized_call(self):
+        pass
+
+    def test_recover_reward_funds(self):
+        pass
+
+
+class Test_set_reward_rate:
+    def test_unauthorized_call(self):
+        pass
+
+    def test_set_reward_rate(self):
+        pass
+
+
+class Test_add_rewards:
+    def test_invalid_reward(self):
+        pass
+
+    def test_add_rewards(self):
+        pass
 
 
 class Test_deposit:
@@ -112,17 +253,6 @@ class Test_deposit:
 
     def test_successful_deposit_without_lockup(self):
         pass
-
-
-class Test_view_functions:
-    # @pytest.fixture(scope='function', autouse=True)
-    # def setup(self):
-    #     # create a deposit here
-    #     pass
-
-    class Test_get_num_deposits:
-        def test_get_num_deposits(self):
-            pass
 
 
 class Test_initiate_cooldown:
