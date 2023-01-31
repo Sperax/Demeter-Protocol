035bbcd07d32c89e2ebb63ba7799ff9f33a28a6c
YashP16 <yash.pitroda786@gmail.com>
2023-01-31 13:21:45 +0530

    feat(scripts): Add following deployment configurations:
    * SushiSwap
    * TraderJoe
    * Camelot

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,6 +2,8 @@ from brownie import (
     FarmFactory,
     Demeter_UniV3FarmDeployer_v2,
     Demeter_UniV3Farm_v2,
+    Demeter_UniV2FarmDeployer,
+    Demeter_E20_farm,
     chain,
 )
 
@@ -137,6 +139,73 @@ deployment_config = {
             ]
         )
     ),
+    'CamelotFarmDeployer_v1': Deployment_data(
+        contract=Demeter_UniV2FarmDeployer,
+        config=Deployment_config(
+            upgradeable=False,
+            deployment_params={
+                'farm_factory': '0xC4fb09E0CD212367642974F6bA81D8e23780A659',
+                'camelot_factory':
+                    '0x6EcCab422D763aC031210895C81787E87B43A652',
+                'deployer_name': 'Demeter_Camelot_Farm'
+            },
+            post_deployment_steps=[
+                Step(
+                    func='transferOwnership',
+                    transact=True,
+                    args={
+                        'new_owner':
+                            '0x6d5240f086637fb408c7F727010A10cf57D51B62'
+                    }
+                ),
+            ]
+        )
+    ),
+
+    'SushiSwapFarmDeployer_v1': Deployment_data(
+        contract=Demeter_UniV2FarmDeployer,
+        config=Deployment_config(
+            upgradeable=False,
+            deployment_params={
+                'farm_factory': '0xC4fb09E0CD212367642974F6bA81D8e23780A659',
+                'SushiSwap_factory':
+                    '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+                'deployer_name': 'Demeter_SushiSwap_Farm'
+            },
+            post_deployment_steps=[
+                Step(
+                    func='transferOwnership',
+                    transact=True,
+                    args={
+                        'new_owner':
+                            '0x6d5240f086637fb408c7F727010A10cf57D51B62'
+                    }
+                ),
+            ]
+        )
+    ),
+    'TraderJoeFarmDeployer_v1': Deployment_data(
+        contract=Demeter_UniV2FarmDeployer,
+        config=Deployment_config(
+            upgradeable=False,
+            deployment_params={
+                'farm_factory': '0xC4fb09E0CD212367642974F6bA81D8e23780A659',
+                'SushiSwap_factory':
+                    '0xaE4EC9901c3076D0DdBe76A520F9E90a6227aCB7',
+                'deployer_name': 'Demeter_TraderJoe_Farm'
+            },
+            post_deployment_steps=[
+                Step(
+                    func='transferOwnership',
+                    transact=True,
+                    args={
+                        'new_owner':
+                            '0x6d5240f086637fb408c7F727010A10cf57D51B62'
+                    }
+                ),
+            ]
+        )
+    ),
 }
 
 upgrade_config = {}
@@ -151,7 +220,7 @@ farm_config = {
                 'farm_admin': '0x5b12d9846F8612E439730d18E1C12634753B1bF1',
                 'farm_start_time': chain.time() + 100,
                 'cooldown_period': 0,
-                'uniswap_pool_data': {
+                'pool_data': {
                     'token_A': '0x2CaB3abfC1670D1a452dF502e216a66883cDf079',
                     'token_B': '0xD74f5255D557944cf7Dd0E45FF521520002D5748',
                     'fee_tier': 3000,
@@ -168,5 +237,22 @@ farm_config = {
                 ]
             }
         )
-    )
+    ),
+    'usds_usdc_camelot_v1': Create_Farm_data(
+        contract=Demeter_E20_farm,
+        deployer_contract=Demeter_UniV2FarmDeployer,
+        deployer_address='0x1a85c90cfEE9eD499C598a11ea56A8E5a16c307f',
+        config=Farm_config(
+            deployment_params={
+                'farm_admin': '0x5b12d9846F8612E439730d18E1C12634753B1bF1',
+                'farm_start_time': chain.time() + 1000,
+                'cooldown_period': 0,
+                'pool_data': {
+                    'token_A': '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+                    'token_B': '0xD74f5255D557944cf7Dd0E45FF521520002D5748',
+                },
+                'reward_token_data': []
+            }
+        )
+    ),
 }
