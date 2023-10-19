97c28d5e9a19565edb390ec4ff33f7b3bc69199a
TechnoGeek01 <rsudharsangames@gmail.com>
2023-10-19 19:05:54 +0530

    integrated sushiswap V3 into demeter

---
+++ contracts/sushiswapV3/Demeter_SushiV3FarmDeployer.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
+//@@@@@@@@&....(@@@@@@@@@@@@@..../@@@@@@@@@//
+//@@@@@@........../@@@@@@@........../@@@@@@//
+//@@@@@............(@@@@@............(@@@@@//
+//@@@@@(............@@@@@(...........&@@@@@//
+//@@@@@@@...........&@@@@@@.........@@@@@@@//
+//@@@@@@@@@@@@@@%..../@@@@@@@@@@@@@@@@@@@@@//
+//@@@@@@@@@@@@@@@@@@@...@@@@@@@@@@@@@@@@@@@//
+//@@@@@@@@@@@@@@@@@@@@@......(&@@@@@@@@@@@@//
+//@@@@@@#.........@@@@@@#...........@@@@@@@//
+//@@@@@/...........%@@@@@............%@@@@@//
+//@@@@@............#@@@@@............%@@@@@//
+//@@@@@@..........#@@@@@@@/.........#@@@@@@//
+//@@@@@@@@@&/.(@@@@@@@@@@@@@@&/.(&@@@@@@@@@//
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
+
+import {BaseFarmDeployer, IFarmFactory} from "../BaseFarmDeployer.sol";
+import {Demeter_SushiV3Farm, RewardTokenData, SushiswapPoolData} from "./Demeter_SushiV3Farm.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Demeter_SushiV3FarmDeployer is BaseFarmDeployer, ReentrancyGuard {
+    // farmAdmin - Address to which ownership of farm is transferred to post deployment
+    // farmStartTime - Time after which the rewards start accruing for the deposits in the farm.
+    // cooldownPeriod -  cooldown period for locked deposits (in days)
+    //                   make cooldownPeriod = 0 for disabling lockup functionality of the farm.
+    // sushiswapPoolData - Init data for SushiswapV3 pool.
+    //                  (tokenA, tokenB, feeTier, tickLower, tickUpper)
+    // rewardTokenData - [(rewardTokenAddress, tknManagerAddress), ... ]
+    struct FarmData {
+        address farmAdmin;
+        uint256 farmStartTime;
+        uint256 cooldownPeriod;
+        SushiswapPoolData sushiswapPoolData;
+        RewardTokenData[] rewardData;
+    }
+
+    string public constant DEPLOYER_NAME = "Demeter_SushiV3FarmDeployer_v3";
+
+    constructor(address _factory) BaseFarmDeployer() {
+        _isNonZeroAddr(_factory);
+        factory = _factory;
+        discountedFee = 50e18; // 50 USDs
+        farmImplementation = address(new Demeter_SushiV3Farm());
+    }
+
+    /// @notice Deploys a new SushiswapV3 farm.
+    /// @param _data data for deployment.
+    function createFarm(FarmData memory _data)
+        external
+        nonReentrant
+        returns (address)
+    {
+        _isNonZeroAddr(_data.farmAdmin);
+        Demeter_SushiV3Farm farmInstance = Demeter_SushiV3Farm(
+            Clones.clone(farmImplementation)
+        );
+        farmInstance.initialize(
+            _data.farmStartTime,
+            _data.cooldownPeriod,
+            _data.sushiswapPoolData,
+            _data.rewardData
+        );
+        farmInstance.transferOwnership(_data.farmAdmin);
+        address farm = address(farmInstance);
+        // Calculate and collect fee if required
+        _collectFee(
+            _data.sushiswapPoolData.tokenA,
+            _data.sushiswapPoolData.tokenB
+        );
+        emit FarmCreated(farm, msg.sender, _data.farmAdmin);
+        IFarmFactory(factory).registerFarm(farm, msg.sender);
+        return farm;
+    }
+}
---
+++ contracts/sushiswapV3/tests/SushiswapV3Test.sol
@@ -0,0 +1,55 @@
+// This contract is for testing purpose (Arbitrum)
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+pragma experimental ABIEncoderV2;
+
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ISwapRouter} from "./ISwapRouter.sol";
+import {INonfungiblePositionManager} from "../interfaces/UniswapV3.sol";
+
+/**
+ * @title Token Swapping on SushiswapV3 Arbitrum
+ * @notice Sushiswap V3 is a fork of Uniswap V3 with some modifications so the below reference is still valid
+ * @dev reference: https://docs.uniswap.org/protocol/guides/swaps/single-swaps
+ * @author Sperax Foundation
+ */
+contract SushiswapV3Test {
+    using SafeERC20 for IERC20;
+    INonfungiblePositionManager public nonfungiblePositionManager =
+        INonfungiblePositionManager(0xF0cBce1942A68BEB3d1b73F0dd86C8DCc363eF49);
+
+    ISwapRouter public constant SWAP_ROUTER =
+        ISwapRouter(0x8A21F6768C1f8075791D08546Dadf6daA0bE820c);
+
+    event SwapTest(
+        address inputToken,
+        address outputToken,
+        uint256 amountIn,
+        uint256 amountOut
+    );
+
+    /**
+     * @notice swaps a fixed amount of inputToken for a maximum possible amount of outputToken on Sushiswap V3
+     */
+    function swap(
+        address inputToken,
+        address outputToken,
+        uint24 poolFee,
+        uint256 amountIn
+    ) external returns (uint256 amountOut) {
+        IERC20(inputToken).safeApprove(address(SWAP_ROUTER), amountIn);
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: inputToken,
+                tokenOut: outputToken,
+                fee: poolFee,
+                recipient: msg.sender,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        // Executes the swap.
+        amountOut = SWAP_ROUTER.exactInputSingle(params);
+        emit SwapTest(inputToken, outputToken, amountIn, amountOut);
+    }
+}
