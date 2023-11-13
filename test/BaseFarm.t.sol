dce3ff293cc565981b23bf5705ec5e5686468ad0
Dayaa <m.d.e.boutalbi@me.com>
2023-11-13 19:06:50 +0100

    fix(BaseFarm): Minor Prank Fix

--- test/BaseFarm.t.sol
+++ test/BaseFarm.t.sol
@@ -76,7 +76,7 @@ abstract contract BaseFarmTest is TestNetworkConfig {
 
     function setUp() public virtual override {
         super.setUp();
-        user = actors[4];
+        user = actors[0];
         newTokenManager = actors[3];
     }
 
