42ca899aed944f11d0d144754a3251c1d2380f25
Sudharsan R <34187497+TechnoGeek01@users.noreply.github.com>
2024-01-31 14:29:50 +0530

    Natspec update
    Co-authored-by: arcantheon <140178288+arcantheon@users.noreply.github.com>

--- contracts/features/BaseFarmWithExpiry.sol
+++ contracts/features/BaseFarmWithExpiry.sol
@@ -21,7 +21,7 @@ abstract contract BaseFarmWithExpiry is BaseFarm {
     error FarmNotYetStarted();
 
     /// @notice Update the farm end time.
-    /// @dev Can be updated only before the farm expired or closed
+    /// @dev Can be updated only before the farm expired or closed.
     ///      Extension should be incremented in multiples of 1 USDs/day with minimum of 100 days at a time and a maximum of 300 days.
     ///      extension is possible only after farm started
     /// @param _extensionDays The number of days to extend the farm
