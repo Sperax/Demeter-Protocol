8a7349e1d5eaa330544692f7eac1cfca0f62525c
mehultuteja <arcantheon@gmail.com>
2024-02-02 04:15:35 +0530

    Usage of storage variables for gas optimization

--- contracts/OperableDeposit.sol
+++ contracts/OperableDeposit.sol
@@ -18,6 +18,7 @@ pragma solidity 0.8.16;
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 
 import {BaseFarmStorage} from "./BaseFarmStorage.sol";
+import {Subscription, RewardFund} from "./interfaces/DataTypes.sol";
 
 abstract contract OperableDeposit is BaseFarmStorage {
     uint256 public constant PRECISION = 1e18;
@@ -32,17 +33,19 @@ abstract contract OperableDeposit is BaseFarmStorage {
     /// @param _amount _amount to be increased.
     function _updateSubscriptionForIncrease(uint256 _depositId, uint256 _amount) internal {
         uint256 numRewards = rewardTokens.length;
-        uint256 numSubs = subscriptions[_depositId].length;
+        Subscription[] storage _subscriptions = subscriptions[_depositId];
+        RewardFund[] storage _rewardFunds = rewardFunds;
+        uint256 numSubs = _subscriptions.length;
         for (uint256 iSub; iSub < numSubs;) {
-            uint256[] storage _rewardDebt = subscriptions[_depositId][iSub].rewardDebt;
-            uint8 _fundId = subscriptions[_depositId][iSub].fundId;
+            uint256[] storage _rewardDebt = _subscriptions[iSub].rewardDebt;
+            uint8 _fundId = _subscriptions[iSub].fundId;
             for (uint8 iRwd; iRwd < numRewards;) {
-                _rewardDebt[iRwd] += ((_amount * rewardFunds[_fundId].accRewardPerShare[iRwd]) / PRECISION);
+                _rewardDebt[iRwd] += ((_amount * _rewardFunds[_fundId].accRewardPerShare[iRwd]) / PRECISION);
                 unchecked {
                     ++iRwd;
                 }
             }
-            rewardFunds[_fundId].totalLiquidity += _amount;
+            _rewardFunds[_fundId].totalLiquidity += _amount;
             unchecked {
                 ++iSub;
             }
@@ -54,17 +57,19 @@ abstract contract OperableDeposit is BaseFarmStorage {
     /// @param _amount _amount to be decreased.
     function _updateSubscriptionForDecrease(uint256 _depositId, uint256 _amount) internal {
         uint256 numRewards = rewardTokens.length;
-        uint256 numSubs = subscriptions[_depositId].length;
+        Subscription[] storage _subscriptions = subscriptions[_depositId];
+        RewardFund[] storage _rewardFunds = rewardFunds;
+        uint256 numSubs = _subscriptions.length;
         for (uint256 iSub; iSub < numSubs;) {
-            uint256[] storage _rewardDebt = subscriptions[_depositId][iSub].rewardDebt;
-            uint8 _fundId = subscriptions[_depositId][iSub].fundId;
+            uint256[] storage _rewardDebt = _subscriptions[iSub].rewardDebt;
+            uint8 _fundId = _subscriptions[iSub].fundId;
             for (uint8 iRwd; iRwd < numRewards;) {
-                _rewardDebt[iRwd] -= ((_amount * rewardFunds[_fundId].accRewardPerShare[iRwd]) / PRECISION);
+                _rewardDebt[iRwd] -= ((_amount * _rewardFunds[_fundId].accRewardPerShare[iRwd]) / PRECISION);
                 unchecked {
                     ++iRwd;
                 }
             }
-            rewardFunds[_fundId].totalLiquidity -= _amount;
+            _rewardFunds[_fundId].totalLiquidity -= _amount;
             unchecked {
                 ++iSub;
             }
