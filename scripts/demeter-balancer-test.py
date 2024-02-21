8f55aa4c2adafeccd2c4808491306f798ab7b4a8
Dayaa <m.d.e.boutalbi@me.com>
2024-02-21 12:56:26 +0100

    Refactor demeter-balancer-test.py script

--- scripts/demeter-balancer-test.py
+++ scripts/demeter-balancer-test.py
@@ -1,19 +1,49 @@
-# from brownie import (
-#     Demeter_BalancerFarm_Deployer,
-#     chain,
-#     interface
-# )
+
 from ape import project, chain, accounts
+from dataclasses import dataclass
+
 
 factory = "0xC4fb09E0CD212367642974F6bA81D8e23780A659"
 balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
+
 OWNER = accounts["0x6d5240f086637fb408c7F727010A10cf57D51B62"]
 USDs_Address = "0xD74f5255D557944cf7Dd0E45FF521520002D5748"
 usdsFunder = accounts["0x5c9d09716404556646B0B4567Cb4621C18581f94"]
-USDs = project.dependencies["OpenZeppelin"]["4.9.5"].IERC20.at(USDs_Address)
 
+USDs = project.dependencies["OpenZeppelin"]["4.9.3"].IERC20.at(USDs_Address)
+
+
+@dataclass
+class FarmData:
+    farmAdmin: bytes
+    farmStartTime: int
+    cooldownPeriod: int
+    poolId: bytes
+    rewardData: list
+    def getData(self):
+        return (
+            self.farmAdmin,
+            self.farmStartTime,
+            self.cooldownPeriod,
+            self.poolId,
+            self.rewardData,
+        )
+@dataclass
+class RewardTokenData:
+    token: bytes
+    tknManager: bytes
 
 def createFarm(farmDeployer, creatorAddress):
+    """
+    Creates a farm using the provided farm deployer and creator address.
+
+    Args:
+        farmDeployer: The farm deployer contract instance.
+        creatorAddress: The address of the creator.
+
+    Returns:
+        The transaction object for creating the farm.
+    """
     creator = accounts[creatorAddress]
     creator.balance += int(10e18)
     usdsFunder.balance += int(10e18)
@@ -32,20 +62,29 @@ def createFarm(farmDeployer, creatorAddress):
     USDs.transfer(creator, int(100e18), sender=usdsFunder)
     USDs.approve(farmDeployer, int(100e18), sender=creator)
     project.IFarmFactory.at(factory).registerFarmDeployer(farmDeployer, sender=OWNER)
+
+    rewardTknData = RewardTokenData(USDs.address, creator.address)
+    data = FarmData(
+        OWNER.address,
+        chain.pending_timestamp,
+        0,
+        "0x423a1323c871abc9d89eb06855bf5347048fc4a5000000000000000000000496",
+        []
+    )
     tx = farmDeployer.createFarm(
-        (
-            params["farm-admin"].address,
-            params["farm-start-time"],
-            params["cooldown-period"],
-            params["pool-id"],
-            params["reward-token-data"],
-        ),
-        sender=creator,
+        data,
+        sender=OWNER,
     )
     return tx
 
 
 def main():
+    """
+    The main function that deploys the farm deployer and creates a farm.
+
+    Returns:
+        None
+    """
     # creator = input("Enter user's address: ")
     creator = "0x12DBb60bAd909e6d9139aBd61D0c9AA11eB49D51"
     OWNER.balance += int(10e18)
