cdc258e27ff28fea9006d86498fa4884567f1c29
mehultuteja <reachmehultuteja@gmail.com>
2024-02-25 00:41:26 +0530

    Added fixed APR struct in DataTypes

--- contracts/interfaces/DataTypes.sol
+++ contracts/interfaces/DataTypes.sol
@@ -55,3 +55,17 @@ struct RewardData {
     uint8 id;
     uint256 accRewardBal;
 }
+
+// Configuration for fixed APR reward tokens.
+// apr - APR of the reward.
+// maxRewardsPerSec - Maximum amount of tokens to be emitted per second.
+// baseTokens - Addresses of tokens to be considered for calculating the L value.
+// tokenManager - Account that manages the rewardToken.
+// nonLockupRewardPer - Reward percentage allocation for no lockup fund (rest goes to lockup fund).
+struct FixedAPRRewardConfig {
+    uint256 apr;
+    uint256 maxRewardsPerSec;
+    address[] baseTokens;
+    address tokenManager;
+    uint256 noLockupRewardPer; // 5000 = 50%
+}
