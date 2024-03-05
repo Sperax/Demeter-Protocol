ecd3155ae8c7ef3eb03568f37f60316292c5f0ae
arcantheon <arcantheon@gmail.com>
2024-03-05 17:34:04 +0530

    Removed farm registered function from interface

--- contracts/interfaces/IFarmRegistry.sol
+++ contracts/interfaces/IFarmRegistry.sol
@@ -12,6 +12,4 @@ interface IFarmRegistry {
         returns (address feeFeceiver, address feeToken, uint256 feeAmount, uint256 extensionFeePerDay);
 
     function isPrivilegedDeployer(address _user) external view returns (bool);
-
-    function farmRegistered(address farm) external view returns (bool);
 }
